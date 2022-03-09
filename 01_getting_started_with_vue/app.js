425bd3cad6084362074db9bbf8b19cd12cf779f8
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-03-09 19:35:27 +0630

    Using vue events

--- 01_getting_started_with_vue/app.js
+++ 01_getting_started_with_vue/app.js
@@ -5,11 +5,19 @@ const vm = Vue.createApp({
             lastName: "Doe",
             url: "https://facebook.com",
             raw_data: "<h1>Hello</h1>",
+            count: 0,
         };
     },
     methods: {
         fullname() {
             return `${this.firstName} ${this.lastName}`;
+        },
+        add() {
+            this.count = this.count + 1;
+        },
+        reduce(event) {
+            console.log(event.target);
+            this.count -= 1;
         },
     },
 }).mount("#app");
