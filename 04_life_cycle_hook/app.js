f357bffc77b40604454c0330c0a39c8afa998461
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-03-10 04:25:16 +0630

    Using proxy

--- 04_life_cycle_hook/app.js
+++ 04_life_cycle_hook/app.js
@@ -1,33 +1,47 @@
-const vm = Vue.createApp({
-    data() {
-        return {
-            msg: "hi there",
-        };
-    },
-    beforeCreate() {
-        console.log("beforeCreate()", this.msg);
-    },
-    created() {
-        console.log("created()", this.msg);
-    },
-    beforeMount() {
-        console.log("beforeMount()", this.$el);
-    },
-    mounted() {
-        console.log("mounted()", this.$el);
-    },
-    beforeUpdate() {
-        console.log("beforeUpdate()");
-    },
-    updated() {
-        console.log("updated()");
-    },
-    beforeUnmount() {
-        console.log("beforeUnmount()");
-    },
-    unmounted() {
-        console.log("unmounted()");
+// const vm = Vue.createApp({
+//     data() {
+//         return {
+//             msg: "hi there",
+//         };
+//     },
+//     beforeCreate() {
+//         console.log("beforeCreate()", this.msg);
+//     },
+//     created() {
+//         console.log("created()", this.msg);
+//     },
+//     beforeMount() {
+//         console.log("beforeMount()", this.$el);
+//     },
+//     mounted() {
+//         console.log("mounted()", this.$el);
+//     },
+//     beforeUpdate() {
+//         console.log("beforeUpdate()");
+//     },
+//     updated() {
+//         console.log("updated()");
+//     },
+//     beforeUnmount() {
+//         console.log("beforeUnmount()");
+//     },
+//     unmounted() {
+//         console.log("unmounted()");
+//     },
+// });
+
+// vm.mount("#app");
+
+const data = {
+    name: "Jonny",
+};
+
+const observedData = new Proxy(data, {
+    set(taraget, key, value) {
+        document.getElementById("app").innerText = value;
+        taraget[key] = value;
     },
 });
 
-vm.mount("#app");
+observedData.name = "some";
+console.log(observedData.name);
