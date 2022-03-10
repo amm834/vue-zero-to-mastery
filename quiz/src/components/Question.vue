add2377d306460eafef4b3709e64a5519d73cf17
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-03-11 04:45:45 +0630

    Moving between questions

--- quiz/src/components/Question.vue
+++ quiz/src/components/Question.vue
@@ -1,13 +1,24 @@
 <template>
     <div class="questions-ctr">
         <div class="progress">
-            <div class="bar"></div>
-            <div class="status">1 out of 3 questions answered</div>
+            <div class="bar"
+            :style="{width: `${(questionsAnswered / questions.length )* 100}%`}"
+            ></div>
+            <div class="status">{{questionsAnswered}} out of {{questions.length}} questions answered</div>
         </div>
-        <div class="single-question" v-for="question in questions">
+        <div class="single-question"
+             v-for="(question,index) in questions"
+             :key="question.q"
+             v-show="index === questionsAnswered"
+        >
             <div class="question">{{ question.q }}</div>
             <div class="answers">
-                <div class="answer" v-for="answer in question.answers">{{answer.text}}</div>
+                <div class="answer"
+                     v-for="answer in question.answers"
+                     :key="answer.text"
+                     @click="onQuestionAnswered(answer.is_correct)"
+                >{{ answer.text }}
+                </div>
             </div>
         </div>
     </div>
@@ -16,7 +27,13 @@
 <script>
 export default {
     name: "Question",
-    props: ['questions']
+    props: ['questions', 'questionsAnswered'],
+    emits:['question-answered'],
+    methods: {
+        onQuestionAnswered(is_correct) {
+            this.$emit('question-answered', is_correct)
+        }
+    }
 }
 </script>
 
