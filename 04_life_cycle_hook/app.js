ef59c21eee2cbd57a6cd9011530cdb4ac337b8dc
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-03-10 21:00:48 +0630

    Using vue main component

--- 04_life_cycle_hook/app.js
+++ 04_life_cycle_hook/app.js
@@ -1,47 +1,56 @@
-// const vm = Vue.createApp({
-//     data() {
-//         return {
-//             msg: "hi there",
-//         };
-//     },
-//     beforeCreate() {
-//         console.log("beforeCreate()", this.msg);
-//     },
-//     created() {
-//         console.log("created()", this.msg);
-//     },
-//     beforeMount() {
-//         console.log("beforeMount()", this.$el);
-//     },
-//     mounted() {
-//         console.log("mounted()", this.$el);
-//     },
-//     beforeUpdate() {
-//         console.log("beforeUpdate()");
-//     },
-//     updated() {
-//         console.log("updated()");
-//     },
-//     beforeUnmount() {
-//         console.log("beforeUnmount()");
-//     },
-//     unmounted() {
-//         console.log("unmounted()");
-//     },
-// });
-
-// vm.mount("#app");
-
-const data = {
-    name: "Jonny",
-};
-
-const observedData = new Proxy(data, {
-    set(taraget, key, value) {
-        document.getElementById("app").innerText = value;
-        taraget[key] = value;
+const vm = Vue.createApp({
+    data() {
+        return {
+            msg: "hi there",
+        };
+    },
+    beforeCreate() {
+        console.log("beforeCreate()", this.msg);
+    },
+    created() {
+        console.log("created()", this.msg);
+    },
+    beforeMount() {
+        console.log("beforeMount()", this.$el);
+    },
+    mounted() {
+        console.log("mounted()", this.$el);
+    },
+    beforeUpdate() {
+        console.log("beforeUpdate()");
+    },
+    updated() {
+        console.log("updated()");
+    },
+    beforeUnmount() {
+        console.log("beforeUnmount()");
+    },
+    unmounted() {
+        console.log("unmounted()");
     },
 });
 
-observedData.name = "some";
-console.log(observedData.name);
+vm.mount("#app");
+
+vm.component("hello", {
+    template: "<h1> {{ msg }}</h1>",
+    data() {
+        return {
+            msg: "hello world",
+        };
+    },
+});
+
+// const data = {
+//     name: "Jonny",
+// };
+
+// const observedData = new Proxy(data, {
+//     set(taraget, key, value) {
+//         document.getElementById("app").innerText = value;
+//         taraget[key] = value;
+//     },
+// });
+
+// observedData.name = "some";
+// console.log(observedData.name);
