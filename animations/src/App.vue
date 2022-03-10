f9f87b5c952e03167501f05dd9c1707984e136cb
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-03-11 03:14:13 +0630

    Using animation in vue

--- animations/src/App.vue
+++ animations/src/App.vue
@@ -1,9 +1,14 @@
 <template>
     <button @click="flag = !flag">Toggle</button>
-    <transition name="fade" mode="out-in">
-        <h1 v-if="flag">Hello World!</h1>
-        <h1 v-else>Another Hello World</h1>
+    <!--    <transition name="fade" mode="out-in">
+            <h1 v-if="flag">Hello World!</h1>
+            <h1 v-else>Another Hello World</h1>
+        </transition>-->
+
+    <transition name="zoom" type="animation" appear>
+        <h1>Hello World</h1>
     </transition>
+
 </template>
 
 <script>
@@ -18,6 +23,12 @@ export default {
 </script>
 
 <style>
+h1 {
+    width: 400px;
+    padding: 20px;
+    margin: 20px;
+}
+
 .fade-enter-from {
     opacity: 0;
 }
@@ -30,4 +41,34 @@ export default {
     opacity: 0;
     transition: all 3s linear;
 }
+
+@keyframes zoom-in {
+    from {
+        transform: scale(0, 0);
+    }
+    to {
+        transform: scale(1, 1);
+    }
+}
+
+@keyframes zoom-out {
+    from {
+        transform: scale(1, 1);
+    }
+    to {
+        transform: scale(0, 0);
+    }
+}
+
+
+.zoom-enter-active {
+    animation: zoom-in 1s linear forwards;
+    transition: all 2s linear;
+}
+
+.zoom-leave-active {
+    animation: zoom-out 1s linear forwards;
+    transition: all 2s linear;
+}
+
 </style>
