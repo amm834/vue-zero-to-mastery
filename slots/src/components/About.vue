4b5f66b1555b64375eba6cd79b17cf5680cc714d
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-03-11 02:10:22 +0630

    Using dynamic component

---
+++ slots/src/components/About.vue
@@ -0,0 +1,13 @@
+<template>
+<h1>About Component</h1>
+</template>
+
+<script>
+export default {
+    name: "AppAbout"
+}
+</script>
+
+<style scoped>
+
+</style>
--- slots/src/components/Form.vue
+++ slots/src/components/Form.vue
@@ -1,9 +1,14 @@
 <template>
-    <div>
-        <slot>
-            No form to render
-        </slot>
+   <div class="help">
+       <slot name="help"></slot>
+   </div>
+    <div class="fields">
+        <slot name="fields"></slot>
     </div>
+    <div class="buttons">
+        <slot name="buttons"></slot>
+    </div>
+    <slot></slot>
 </template>
 
 <script>
