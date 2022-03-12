d0a7d22182e00a349fce0ef6cf3914e248e70993
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-03-13 05:33:27 +0630

    Using named route

--- music/src/router/index.js
+++ music/src/router/index.js
@@ -1,10 +1,30 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/views/Home";
+import About from "@/views/About";
+import Manage from "@/views/Manage";
 
-const routes = [];
+const routes = [
+    {
+        name: "home",
+        path: "/",
+        component: Home
+    },
+    {
+        name: "about",
+        path: "/about",
+        component: About
+    },
+    {
+        name: "manage",
+        path: "/manage",
+        component: Manage
+    }
+];
 
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
-    routes
+    routes,
+    linkExactActiveClass: "text-yellow-500"
 });
 
 export default router;
