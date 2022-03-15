9dd0a06b6b9b3e881b16992c45070a65d09a227f
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-03-15 19:25:48 +0630

    Using mixin

---
+++ composition/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="app">
+        <div class="scrollPosition">{{ offset }}</div>
+    </div>
+</template>
+
+<script>
+import mixin from "./mixin";
+
+export default {
+    name: "App",
+    mixins: [ mixin ],
+    data() {
+        return {
+            offset: 0
+        }
+    },
+    mounted() {
+        console.log('App is mounted');
+    }
+}
+
+</script>
+
+<style>
+#app {
+    width: 100%;
+    height: 2000px;
+    position: relative;
+}
+
+.scrollPosition {
+    position: fixed;
+}
+</style>
---
+++ composition/src/mixin.js
@@ -0,0 +1,15 @@
+export default {
+    data() {
+        return {
+            offset: 0,
+        };
+    },
+    methods: {
+        update() {
+            this.offset = window.pageYOffset;
+        },
+    },
+    mounted() {
+        window.addEventListener("scroll", this.update);
+    },
+};
