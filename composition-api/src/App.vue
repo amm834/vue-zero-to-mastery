6ce4bb7a4b542e66b1c0f7ec5bf3008ffb1afd2b
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-03-15 21:25:16 +0630

    Using watcher

--- composition-api/src/App.vue
+++ composition-api/src/App.vue
@@ -4,10 +4,15 @@
     <button @click="increment">Update</button>
 
     <p>{{ name }}</p>
+
+    <p>
+      <input type="text" v-model="phrase" />
+      <div>{{ reversePhrase }}</div>
+    </p>
   </div>
 </template>
 
-<script>import { reactive, ref, toRefs } from "vue";
+<script>import { reactive, ref, toRefs, watch } from "vue";
 
 export default {
   name: "App",
@@ -22,10 +27,21 @@ export default {
       user.name = 'Doe'
     }, 2000)
 
+    let phrase = ref("")
+    let reversePhrase = ref("")
+
+    watch(() => {
+      reversePhrase.value = phrase.value.split("").reverse().join("")
+    })
+
+
+
     return {
       num,
       increment,
-      ...toRefs(user)
+      ...toRefs(user),
+      phrase,
+      reversePhrase
     }
   }
 }
