1de54f7b6e657e6cc64946f239ba70b02a923eb2
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-03-15 21:14:18 +0630

    Reactive object

--- composition-api/src/App.vue
+++ composition-api/src/App.vue
@@ -2,22 +2,33 @@
   <div>
     <p>{{ num }}</p>
     <button @click="increment">Update</button>
+
+    <p>{{ name }}</p>
   </div>
 </template>
 
-<script>import { ref } from "vue";
+<script>import { reactive, ref, toRefs } from "vue";
 
 export default {
   name: "App",
   setup() {
     let num = ref(0);
     const increment = () => num.value++;
+    const user = reactive({
+      name: 'John'
+    })
+
+    setTimeout(() => {
+      user.name = 'Doe'
+    }, 2000)
 
     return {
       num,
-      increment
+      increment,
+      ...toRefs(user)
     }
   }
 }
+
 
 </script>
