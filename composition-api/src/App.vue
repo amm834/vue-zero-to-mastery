a574732cd1a242c31dffd5bb8408fa9a8aecfb42
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-03-15 21:03:24 +0630

    Using ref

---
+++ composition-api/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>{{ num }}</p>
+    <button @click="increment">Update</button>
+  </div>
+</template>
+
+<script>import { ref } from "vue";
+
+export default {
+  name: "App",
+  setup() {
+    let num = ref(0);
+    const increment = () => num.value++;
+
+    return {
+      num,
+      increment
+    }
+  }
+}
+
+</script>
